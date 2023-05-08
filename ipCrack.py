db5f9aeb6e86973790b90dfd2fd08c180eb79bb9
Jakub <czaplickijakub1@gmail.com>
2023-05-08 18:19:55 -0400

    Updated Hash

--- ipCrack.py
+++ ipCrack.py
@@ -1,24 +1,20 @@
 import hashlib
 import itertools
 import datetime
-import pandas as pd
 
-hash_table = pd.DataFrame(columns=['IP', 'SHA1', 'SHA224', 'SHA256', 'MD5'])
+hashed_ip = input("Enter the hashed IP address: ")
 
 start_time = datetime.datetime.now()
 
 for ip_tuple in itertools.product(range(256), repeat=4):
     ip_address = ".".join(str(i) for i in ip_tuple)
-
-    hashed_SHA1 = hashlib.sha1(ip_address.encode('utf-8')).hexdigest()
-    hashed_SHA224 = hashlib.sha224 (ip_address.encode('utf-8')).hexdigest()
-    hashed_SHA256 = hashlib.sha256(ip_address.encode('utf-8')).hexdigest()
-    hashed_MD5 = hashlib.md5(ip_address.encode('utf-8')).hexdigest()
-
-    hash_table.loc[len(hash_table)] = [ip_address, hashed_SHA1, hashed_SHA224, hashed_SHA256, hashed_MD5]
-    print(f"Hashing {ip_address}")
-
-hash_table.to_excel('ip_hash_table.xlsx', index=TRUE)
+    hashed = hashlib.md5(ip_address.encode('utf-8')).hexdigest()
+    print(f"Trying {ip_address}")
+    if hashed == hashed_ip:
+        print("Hashed IP address cracked successfully! The original IP address is:", ip_address)
+        break
+else:
+    print("Unable to crack the hashed IP address: hash input error.")
 
 end_time = datetime.datetime.now()
 
